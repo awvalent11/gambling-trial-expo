bd5beabd4dc15d310163fbbe4ed863604b0ed8a5
awvalent11 <andrew.w.valentine7.mil@swf.army.mil>
2024-02-08 13:44:33 -0600

    Adds gluestack and crashes app

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, Pressable} from 'react-native';
+import React from 'react';
+// import {GluestackUIProvider, HStack } from "@gluestack-ui/themed";
+// import {config} from "@gluestack-ui/config"
 export default function App() {
   return (
+  // <GluestackUIProvider config={config}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+              <Text>Welcome to The House!</Text>
+             {/*   <HStack*/}
+             {/*      flexDirection='row'*/}
+             {/*      justifyContent='space-between'*/}
+             {/*      alignItems='center'*/}
+             {/*      paddingHorizontal='$6'*/}
+             {/*      backgroundColor='transparent'*/}
+             {/*  >*/}
+             {/*<Pressable onPress={onPressFunction}*/}
+             {/*    sx={{borderWidth: '2px',*/}
+             {/*    borderTopLeftRadius: '5px',*/}
+             {/*    borderTopRightRadius: '5px', borderBottomLeftRadius: '5px',*/}
+             {/*    borderBottomRightRadius: '5px'}} padding='50px'>*/}
+             {/*  <Text>My Wallet</Text>*/}
+             {/*</Pressable>*/}
+             {/*<Pressable onPress={onPressFunction} padding='50px'>*/}
+             {/*  <Text>NFL</Text>*/}
+             {/*</Pressable>*/}
+             {/*  </HStack>*/}
+              <StatusBar style="auto" />
+     </View>
+   // </GluestackUIProvider>
   );
 }
 
