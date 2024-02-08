a7c9b0ae009672c300c56f342655d8559387c101
awvalent11 <andrew.w.valentine7.mil@swf.army.mil>
2024-02-08 15:02:53 -0600

    Adds shitty padding to NFL & My Wallet Pressables

--- App.js
+++ App.js
@@ -1,34 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Pressable} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-// import {GluestackUIProvider, HStack } from "@gluestack-ui/themed";
-// import {config} from "@gluestack-ui/config"
+import {GluestackUIProvider, HStack, Pressable} from "@gluestack-ui/themed";
+import {config} from "@gluestack-ui/config"
+
 export default function App() {
+  const onPressFunction = () => {
+      console.log("2.5 men season 2 episode 1")
+  }
+
   return (
-  // <GluestackUIProvider config={config}>
+  <GluestackUIProvider config={config}>
     <View style={styles.container}>
-              <Text>Welcome to The House!</Text>
-             {/*   <HStack*/}
-             {/*      flexDirection='row'*/}
-             {/*      justifyContent='space-between'*/}
-             {/*      alignItems='center'*/}
-             {/*      paddingHorizontal='$6'*/}
-             {/*      backgroundColor='transparent'*/}
-             {/*  >*/}
-             {/*<Pressable onPress={onPressFunction}*/}
-             {/*    sx={{borderWidth: '2px',*/}
-             {/*    borderTopLeftRadius: '5px',*/}
-             {/*    borderTopRightRadius: '5px', borderBottomLeftRadius: '5px',*/}
-             {/*    borderBottomRightRadius: '5px'}} padding='50px'>*/}
-             {/*  <Text>My Wallet</Text>*/}
-             {/*</Pressable>*/}
-             {/*<Pressable onPress={onPressFunction} padding='50px'>*/}
-             {/*  <Text>NFL</Text>*/}
-             {/*</Pressable>*/}
-             {/*  </HStack>*/}
+              <Text>Welcome to The House</Text>
+        <HStack padding={'50px'}>
+
+             <Pressable onPress={onPressFunction}
+                 sx={{borderWidth: '2px',
+                 borderTopLeftRadius: '5px',
+                 borderTopRightRadius: '5px', borderBottomLeftRadius: '5px',
+                 borderBottomRightRadius: '5px',
+                 backgroundColor: '$lightBlue200'
+             }} margin='500px' alignItems={'center'} justifyContent={'center'}>
+               <Text>My Wallet</Text>
+             </Pressable>
+             <Pressable onPress={onPressFunction}
+                        sx={{borderWidth: '20px',
+                            borderTopLeftRadius: '5px',
+                            borderTopRightRadius: '5px', borderBottomLeftRadius: '5px',
+                            borderBottomRightRadius: '5px',
+                            backgroundColor: '$coolGray300'
+                        }} padding='500px'>
+               <Text>NFL</Text>
+             </Pressable>
+        </HStack>
               <StatusBar style="auto" />
      </View>
-   // </GluestackUIProvider>
+    </GluestackUIProvider>
   );
 }
 
