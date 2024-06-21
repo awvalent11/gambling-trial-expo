0b51a9a5fffdc6f947dbd331fd20f32666cc55d9
awvalent11@gmail.com <awvalent11@gmail.com>
2024-06-21 12:25:17 -0500

    Parameterizes parsing event_ids for rawOddsApi

--- Scrapers/scrape_live_mlb_betting_pros.py
+++ Scrapers/scrape_live_mlb_betting_pros.py
@@ -4,15 +4,6 @@ from bs4 import BeautifulSoup
 from espn_bet_scraper import scrape_espn_bet
 from human_readable import write_pretty, read_data
 
-def scrape_game_card(html_element):
-    game_line_data = {}
-
-    #scrape school data for each school 'card'
-    #Extract the desired information if the elements exist
-
-    game_line_data = html_element.find('div', class_='wrapper odds-container')
-
-    return game_line_data
 def scrape_betting_pros_mlb_live(target='file',
                url='https://www.bettingpros.com/mlb/odds/moneyline/',
                requests=requests,
@@ -31,16 +22,31 @@ def scrape_betting_pros_mlb_live(target='file',
     #     print("got a 200")
     print(response.status_code)
     # Parse the HTML content of the page
-    print(response.content)
+    # print(response.content)
     soup = BeautifulSoup(response.content, 'html.parser')
     # print(soup.prettify())
     # Extract information
     all_data = []
     print("You're hitting me 1")
-    # rawApi = requests.get("https://api.bettingpros.com/v3/events?sport=MLB&date=2024-06-21", headers={ 'X-Api-Key': "CHi8Hy5CEE4khd46XNYL23dCFX96oUdw6qOt1Dnh"})
-    rawOddsApi = requests.get("https://api.bettingpros.com/v3/offers?sport=MLB&market_id=122&event_id=92956:92835:94376:94885:93323:93168:94038:94167:94595:94177:93877:93008:94136:94906&location=TX&limit=5&page=1", headers = { "X-Api-Key": "CHi8Hy5CEE4khd46XNYL23dCFX96oUdw6qOt1Dnh"})
+    # You can parameterize date here
+    rawApi = requests.get("https://api.bettingpros.com/v3/events?sport=MLB&date=2024-06-21", headers={ 'X-Api-Key': "CHi8Hy5CEE4khd46XNYL23dCFX96oUdw6qOt1Dnh"})
+    event_id_dict = {}
+    date_api_response = rawApi.json()
+    event_id_dict.update(date_api_response)
+    events = event_id_dict["events"]
+    event_id_string = ""
+    for event in events:
+        event_id = event["id"]
+        event_id_string += str(event_id)
+        event_id_string += ":"
+        print("I am the event_id to pass", event_id_string)
+    event_id_string = event_id_string[:-1]
+    print(event_id_string)
+    # scrape event_ids from rawApi to get rawOddsApi
+    api = "https://api.bettingpros.com/v3/offers?sport=MLB&market_id=122&event_id={}&location=TX&limit=5&page=1".format(event_id_string)
+    rawOddsApi = requests.get(api, headers = { "X-Api-Key": "CHi8Hy5CEE4khd46XNYL23dCFX96oUdw6qOt1Dnh"})
     # Using GET request directly to xhr resource on Betting Pros
-    # print(rawOddsApi.content)
+    print(rawOddsApi.content)
     dict = {
 
     }
@@ -49,9 +55,9 @@ def scrape_betting_pros_mlb_live(target='file',
     offers = dict["offers"]
     for offer in offers:
         # participants = offer["participants"]
-        # for participant in participants:
         #     print(participant["id"])
         selections = offer["selections"]
+        # for participant in participants:
         for selection in selections:
             team = selection["label"]
             print("Team name:", team)
@@ -61,3 +67,4 @@ def scrape_betting_pros_mlb_live(target='file',
                 for line in lines:
                     print(line['link'])
                     print(line['cost'])
+
